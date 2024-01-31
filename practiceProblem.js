3fe43fd4c28b7ebf421292c7cb0a5e3982efec9b
Joy <joydas21891920@gmail.com>
2024-01-31 22:29:56 +0600

    second commit

---
+++ practiceProblem.js
@@ -0,0 +1,27 @@
+// Problem 1
+
+/* var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var expense = applePrice + orangePrice;
+var returnMoney = moneyGiven - expense;
+
+console.log(`Total Expence is ${expense}`);
+console.log(`Return Money is ${returnMoney}`);
+
+ */
+
+
+
+// Problem 2
+
+var mathemetics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50
+
+var averageMarks =( (mathemetics + biology + chemistry + physics + bangla)/2); 
+
+console.log(averageMarks);
